7b3a7298903e6205b145f8657f44d798500ac918
Hugo <hugo.pavard92@gmail.com>
2020-02-25 16:11:32 +0100

    connexion/inscription + logo recentré

--- index.php
+++ index.php
@@ -8,13 +8,11 @@
 
 	<?php include "header.php" ?>
 
-	
-
-
-
-
-
-
+	<div class="log">
+		<h2>My Flight Asssistant</h2>
+		<p><a href="">Connexion</a></p>
+		<p><a href="">Inscription</a></p>
+	</div>
 
 	<?php include "footer.php" ?>
 
